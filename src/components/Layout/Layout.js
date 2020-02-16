b140bd5274d2ec87f56f6364f0c7f8873c214203
bouanani-yasser <bouyasser77@gmail.com>
2020-02-16 08:02:58 +0100

    the project construction,add some fonts ...

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Aux from '../../hoc/Auxiliary'
+
+const Layout = (props) =>(
+    <Aux>
+        <div>
+            Logo,ToolBar,...
+        </div>
+        <main>
+            <p> the main content</p>
+        </main>
+
+    </Aux>
+
+
+)
+
+export default Layout;
